f168e4d6782e4f8a72903d4d724ab1da8ae8d3dc
Matt Chad <maciejcha@gmail.com>
2023-11-24 17:40:53 +0800

    Rename files to index for github pages

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,14 +3,14 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1700818273|157680979";
+const CACHE_VERSION = "1700818830|714256220";
 const CACHE_PREFIX = "web-test-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
-const OFFLINE_URL = "web-test.offline.html";
+const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
-const CACHED_FILES = ["web-test.html","web-test.js","web-test.offline.html","web-test.icon.png","web-test.apple-touch-icon.png","web-test.worker.js","web-test.audio.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.worker.js","index.audio.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
-const CACHABLE_FILES = ["web-test.wasm","web-test.pck"];
+const CACHABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHABLE_FILES);
 
 self.addEventListener("install", (event) => {
